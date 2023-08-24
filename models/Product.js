0285779f8763a3960a45c91c596248f32f5811d6
Paola_G <perfectblue@live.com>
2023-08-24 16:57:04 -0700

    added the associations to index.js, added the rest of the fields and rules for Product.js, ProductTag.js, and Tag.js

--- models/Product.js
+++ models/Product.js
@@ -33,6 +33,13 @@ Product.init(
       validate: {
         isNumeric: true,
       },
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "category",
+        key: "id",
+      }
     }
   },
   {
@@ -40,10 +47,8 @@ Product.init(
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product',
+    modelName: "product",
   }
 );
-
-// category id integer
 
 module.exports = Product;
--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,14 +6,29 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      model: "product",
+      key: "id"
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      model: "tag",
+      key: "id"
+    }
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product_tag',
+    modelName: "product_tag",
   }
 );
 
